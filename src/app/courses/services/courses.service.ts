721164f018a0efc4643f3e905f42e99027a9953c
adriano barbosa <adrianobarbosa_4@hotmail.com>
2024-01-29 15:43:13 -0300

    commit 11

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { delay, first, tap } from 'rxjs';
+
 import { Course } from '../model/course';
-import { first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +17,7 @@ export class CoursesService {
     return this.httpClient.get<Course[]>(this.API)
     .pipe(
       first(),
+      delay(5000),
       tap(courses => console.log(courses))
     );
   }
